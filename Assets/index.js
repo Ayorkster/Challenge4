9bf3b5ecb155cddb03a9ebacef14d2f5157ccfdc
mono <traineralaina@gmail.com>
2023-10-23 14:07:01 -0600

    finished js file

--- Assets/index.js
+++ Assets/index.js
@@ -1,7 +1,10 @@
+//button that starts the game
 var start = document.getElementById("start");
 start.addEventListener("click", startGame);
 var timerEL = document.getElementById('timer');
 var timeLeft = 60;
+var highScore = document.getElementById("highScore");
+highScore.addEventListener("click", pastScore);
 
 // Function that controls the Timer
 function setTimer() {
@@ -17,7 +20,128 @@ function setTimer() {
     }, 1000);
 }
 
+//questions
+var questions = [
+    {
+        question: "How many Hit Dice does a Monk have per level?",
+        options: ["1d4", "1d6", "1d8", "1d12"],
+        answerNumber: 2,
+        answer: "1d8",
+    },
+];
+var answerButton = document.getElementsByClassName("answerChoice");
+var correctAnswer = 0;
+var wrongAnswer = 0;
+
+for (var i = 0; i < answerButton.length; i++) {
+    answerButton[i].addEventListener("click", checkAnswer);
+  }
+
+function answerBtn(){
+    var answers = document.getElementById("answerOptions");
+    answers.style.display ="block";
+};
+
+
+
+document.getElementById("answerOptions");
+answerOptions.style.display = "none";
+var currentQuestionNum = 0;
+function showQuestions() {
+    var questionText = document.getElementById("question");
+    var answers = document.getElementById("answerOptions");
+    var currentQ = questions[currentQuestionNum];
+    questionText.textContent = currentQ.question;
+
+    var answerChoice = answers.getElementsByClassName("answerChoice");
+    for( var i=0; 
+        i < currentQ.options.length;
+        i++){
+            answerChoice[i].textContent = currentQ.options[i];
+        }
+};
+function checkAnswer(event) {
+    var selectedAnswer = event.target.textContent;
+    var currentQ = questions[currentQuestionNum];
+  
+    if (selectedAnswer === currentQ.options[currentQ.answerNumber]) {
+        correctAnswer++;
+      } else {
+        wrongAnswer++;
+        timeLeft -= 5;
+      }
+  
+    currentQuestionNum++;
+  
+    if (currentQuestionNum < questions.length) {
+      showQuestions();
+    } else {
+      displayScore();
+    }
+  }
+ 
+  function displayScore() {
+    var score = document.getElementById("score");
+    score.textContent =
+      "Answered correctly: " +
+      correctAnswer +
+      " Answered incorrectly: " +
+      wrongAnswer;
+  
+    var nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("placeholder", "Enter your initials");
+  
+    var submitButton = document.createElement("button");
+    submitButton.textContent = "Submit";
+    
+    score.appendChild(nameInput);
+    score.appendChild(submitButton);
+    nameInput.style.display = "flex";
+    nameInput.style.display = "column";
+
+    var backButton = document.createElement("button");
+    backButton.textContent = "Play again";
+    score.appendChild(backButton);
+    backButton.addEventListener("click", startOver);
+    backButton.style.display = "flex";
+    backButton.style.display = "column";
+
+    submitButton.addEventListener("click", function () {
+      var initials = nameInput.value.trim();
+      if (initials) {
+        localStorage.setItem("userIn", initials);
+        localStorage.setItem("correctAns", correctAnswer);
+      }
+
+    });
+  }
+
+  function pastScore(){
+    var savedIn = localStorage.getItem("userIn");
+    var savedScore = localStorage.getItem("correctAns");
+    if (savedIn){
+       var score = document.getElementById("score");
+       var show = document.getElementById("show");
+       show.style.display = "none";
+        var showIn = document.createElement("p");
+        showIn.textContent = savedIn + " answered " + savedScore + " correctly!"
+        score.appendChild(showIn);
+    }
+    var backButton = document.createElement("button");
+    backButton.textContent = "Play again";
+    score.appendChild(backButton);
+    backButton.addEventListener("click", startOver);
+    backButton.style.display = "flex";
+    backButton.style.display = "column";
+  };
+
+  function startOver() {
+    window.location.reload();
+  }
 // Function that starts the game
 function startGame() {
     setTimer();
-}
+    showQuestions();
+    answerBtn()
+};
